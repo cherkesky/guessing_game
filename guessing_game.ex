a52b6fca4f534e2567eb73ffbdcf7c0ad737d0e6
Cherkesky <guy@cherkesky.com>
2020-08-16 14:52:42 -0500

    bigger and smaller

--- guessing_game.ex
+++ guessing_game.ex
@@ -5,7 +5,24 @@ defmodule GuessingGame do
   # "bigger" -> bigger (low,high)
   # "smaller" -> smaller (low,high)
   # anything else -> "enter valid response"
+
+  def guess(low, high) do
+    IO.puts(low)
+    IO.puts(high)
+  end
+
   def mid(low, high) do
     div(low + high, 2)
   end
+
+  def bigger(low, high) do
+    new_low = min(high, mid(low, high) + 1)
+    guess(new_low, high)
+  end
+
+  def smaller(low, high) do
+    new_high = max(low, mid(low, high) - 1)
+    guess(low, new_high)
+  end
+
 end
